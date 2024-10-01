8b56627b964c17b406bccbf5367707a9d9eae393
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-10-01 20:56:46 +0900

    feature: シーン遷移機能

---
+++ Assets/_Projects/Scripts/General/SceneChange.cs
@@ -0,0 +1,37 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace General 
+{
+    /// <summary>
+    /// シーン変更担当クラス
+    /// </summary>
+    internal sealed class SceneChange 
+    {
+        /// <summary>
+        /// フェードアウト後シーン変更
+        /// </summary>
+        /// <param name="sceneName">遷移先のシーン名</param>
+        /// <returns>UniTask</returns>
+        internal async UniTask FadeOutAndChangeScene(string sceneName)
+        {
+            //await FadeOut();フェードアウトの実装はこちらに
+
+            await LoadSceneAsync(sceneName);
+        }
+
+        /// <summary>
+        /// UniTaskを用いたシーンロード
+        /// </summary>
+        /// <param name="sceneName">遷移先のシーン名</param>
+        /// <returns>UniTask</returns>
+        private async UniTask LoadSceneAsync(string sceneName)
+        {
+            AsyncOperation sceneLoadOperation = SceneManager.LoadSceneAsync(sceneName);
+            while (sceneLoadOperation.isDone == false) await UniTask.Yield();
+        }
+    }
+}
+
+
