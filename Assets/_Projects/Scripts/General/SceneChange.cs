18388c06b3f76b013487c18e845d7f2ce2169e8b
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-10-08 22:54:34 +0900

    refine: シーン変更スクリプトの機能改善
    キャンセレーショントークン　シーンのID化など

--- Assets/_Projects/Scripts/General/SceneChange.cs
+++ Assets/_Projects/Scripts/General/SceneChange.cs
@@ -1,35 +1,50 @@
 ﻿using Cysharp.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンのID
+/// </summary>
+internal enum SceneID
+{
+    Start,
+    Game
+}
 
 namespace General 
 {
     /// <summary>
     /// シーン変更担当クラス
     /// </summary>
-    internal sealed class SceneChange 
+    internal static class SceneChange 
     {
         /// <summary>
         /// フェードアウト後シーン変更
         /// </summary>
-        /// <param name="sceneName">遷移先のシーン名</param>
+        /// <param name="sceneId">遷移先のシーンID</param>
+        /// <param name="cancellationToken">キャンセルトークン</param>
         /// <returns>UniTask</returns>
-        internal async UniTask FadeOutAndChangeScene(string sceneName)
+        internal static async UniTask FadeOutAndChangeScene(SceneID sceneId, CancellationToken cancellationToken)
         {
-            //await FadeOut();フェードアウトの実装はこちらに
+            // await FadeOut(cancellationToken); // フェードアウトの実装はこちらに
 
-            await LoadSceneAsync(sceneName);
+            await LoadSceneAsync(sceneId, cancellationToken);
         }
 
         /// <summary>
         /// UniTaskを用いたシーンロード
         /// </summary>
-        /// <param name="sceneName">遷移先のシーン名</param>
+        /// <param name="sceneId">遷移先のシーン名</param>
+        /// <param name="cancellationToken">キャンセルトークン</param>
         /// <returns>UniTask</returns>
-        private async UniTask LoadSceneAsync(string sceneName)
+        private static async UniTask LoadSceneAsync(SceneID sceneId, CancellationToken cancellationToken)
         {
+            string sceneName = sceneId.ToString();
             AsyncOperation sceneLoadOperation = SceneManager.LoadSceneAsync(sceneName);
-            while (sceneLoadOperation.isDone == false) await UniTask.Yield();
+            sceneLoadOperation.allowSceneActivation = false;
+            await UniTask.WaitUntil(() => sceneLoadOperation.isDone, cancellationToken: cancellationToken);
+            sceneLoadOperation.allowSceneActivation = true;
         }
     }
 }
