5669c9f5bf83fdc4441e961f4a669c73e822a0d5
foriver4725 <hrt67@icloud.com>
2024-10-24 19:25:45 +0900

    feature: 舞台下のボーダーの情報を格納するクラスを作成、ボーダーのアタッチだけした

--- Assets/_Projects/Scripts/Main/EventManager/Borders.cs
+++ Assets/_Projects/Scripts/Main/EventManager/Borders.cs
@@ -8,11 +8,10 @@ namespace Main.EventManager
     [Serializable]
     public sealed class Borders
     {
-        private ReadOnlyCollection<Border> _cachedBusStopCannotMove;
+        private ReadOnlyCollection<Border> _cache;
         [SerializeField, Tooltip("バス停：ここから先には行けない！")]
         private Border[] _busStopCannotMove;
-        public ReadOnlyCollection<Border> BusStopCannotMove
-        { get { _cachedBusStopCannotMove ??= Array.AsReadOnly(_busStopCannotMove); return _cachedBusStopCannotMove; } }
+        public ReadOnlyCollection<Border> BusStopCannotMove { get { _cache ??= Array.AsReadOnly(_busStopCannotMove); return _cache; } }
 
         [SerializeField, Tooltip("橋：きしむ音を立てる")]
         private Border _bridgePlaySound;
@@ -25,6 +24,10 @@ namespace Main.EventManager
         [SerializeField, Tooltip("小道：しゃがんで通り抜ける 2")]
         private TeleportBorder _pathWaySquat2;
         public TeleportBorder PathWaySquat2 => _pathWaySquat2;
+
+        [SerializeField, Tooltip("舞台下：しゃがんで通り抜ける")]
+        private TeleportBorders _underStageSquat;
+        public TeleportBorders UnderStageSquat => _underStageSquat;
 
         [Serializable]
         public sealed class TeleportBorder
@@ -44,6 +47,32 @@ namespace Main.EventManager
             [SerializeField]
             private Transform _secondTf;
             public Transform SecondTf => _secondTf;
+        }
+
+        [Serializable]
+        public sealed class TeleportBorders
+        {
+            private ReadOnlyCollection<Element> _cache;
+
+            [SerializeField]
+            private Element[] _elements;
+            public ReadOnlyCollection<Element> Elements { get { _cache ??= Array.AsReadOnly(_elements); return _cache; } }
+
+            [Serializable]
+            public sealed class Element
+            {
+                [SerializeField]
+                private Border _in;
+
+                [SerializeField]
+                private Border _out;
+
+                [SerializeField]
+                private Transform _inTf;
+
+                [SerializeField]
+                private Transform _outTf;
+            }
         }
     }
 
