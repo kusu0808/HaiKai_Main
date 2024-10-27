71142a4f96f91248d53cd2c83edf34fc63ad93ba
foriver4725 <hrt67@icloud.com>
2024-10-27 23:34:07 +0900

    feature: プレイヤーアイテムのセーブロード

--- Assets/_Projects/Scripts/Main/PlayerItem.cs
+++ Assets/_Projects/Scripts/Main/PlayerItem.cs
@@ -1,52 +1,29 @@
-﻿using General;
-using System;
-using System.Text;
+﻿using System;
+using UnityEngine;
 
 namespace Main
 {
+    [Serializable]
     public sealed class PlayerItem
     {
-        private bool _hasItem1 = false;
-        private bool _hasItem2 = false;
-        private bool _hasItem3 = false;
-
-        // セッタ未実装
-        public bool HasItem1 => _hasItem1;
-        public bool HasItem2 => _hasItem2;
-        public bool HasItem3 => _hasItem3;
+        public bool HasItem1 { get; set; } = false;
+        public bool HasItem2 { get; set; } = false;
+        public bool HasItem3 { get; set; } = false;
 
         public PlayerItem() { }
 
-        public string Write()
+        private static readonly string playerItemKey = "PlayerItem";
+
+        public static void Save(PlayerItem playerItem)
         {
-            StringBuilder sb = new();
-            sb.Append(_hasItem1 ? "1" : "0");
-            sb.Append(_hasItem2 ? "1" : "0");
-            sb.Append(_hasItem3 ? "1" : "0");
-            return sb.ToString();
+            string jsonData = JsonUtility.ToJson(playerItem);
+            ES3.Save(playerItemKey, jsonData);
         }
 
-        public void Read(string id)
+        public static void Load(out PlayerItem playerItem)
         {
-            try
-            {
-                if (id.Length != 3) throw new Exception();
-
-                SetOrThrow(ref _hasItem1, id[0]);
-                SetOrThrow(ref _hasItem2, id[1]);
-                SetOrThrow(ref _hasItem3, id[2]);
-            }
-            catch (Exception)
-            {
-                "読み込み失敗".Warn();
-            }
-
-            static void SetOrThrow(ref bool flag, char c) => flag = c switch
-            {
-                '1' => true,
-                '0' => false,
-                _ => throw new Exception()
-            };
+            string jsonData = ES3.Load<string>(playerItemKey, string.Empty);
+            playerItem = JsonUtility.FromJson<PlayerItem>(jsonData);
         }
     }
 }
