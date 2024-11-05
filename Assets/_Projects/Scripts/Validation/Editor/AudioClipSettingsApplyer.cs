c5952f69ea7bfea4c5e3a45bc2383c392f5fc44e
foriver4725 <hrt67@icloud.com>
2024-11-05 21:41:48 +0900

    refactor: 名前空間追加、wavにも対応
    Windowsでの動作は未確認、サウンドファイルは音量が統一されていないため、多分今後差し替え

--- Assets/_Projects/Scripts/Validation/Editor/AudioClipSettingsApplyer.cs
+++ Assets/_Projects/Scripts/Validation/Editor/AudioClipSettingsApplyer.cs
@@ -6,63 +6,77 @@ using UnityEditor;
 using UnityEngine;
 using General;
 
-public sealed class AudioClipSettingsApplyer : EditorWindow
+namespace Validation
 {
-    private const string FOLDERS_ROOT_PATH = "Assets/_Projects/Sounds";
-    private const string MENU_NAME = "Audio Clip Settings";
-
-    [MenuItem("Tools/Validation/" + MENU_NAME)]
-    public static void ShowWindow() => GetWindow<AudioClipSettingsApplyer>(MENU_NAME);
-
-    private void OnGUI()
+    public sealed class AudioClipSettingsApplyer : EditorWindow
     {
-        EditorGUILayout.LabelField("オーディオクリップ(mp3)", EditorStyles.boldLabel);
+        private const string FOLDERS_ROOT_PATH = "Assets/_Projects/Sounds";
+        private const string MENU_NAME = "Audio Clip Settings";
 
-        if (GUILayout.Button("Soundsフォルダ内の\n全オーディオクリップに設定を適用する"))
+        [MenuItem("Tools/Validation/" + MENU_NAME)]
+        public static void ShowWindow() => GetWindow<AudioClipSettingsApplyer>(MENU_NAME);
+
+        private void OnGUI()
         {
-            Execute();
-        }
-    }
+            EditorGUILayout.LabelField("オーディオクリップ", EditorStyles.boldLabel);
 
-    private static void Execute()
-    {
-        try
+            if (GUILayout.Button("【mp3】\nSoundsフォルダ内の\n全オーディオクリップに設定を適用する")) Execute(SoundFileType.MP3);
+            if (GUILayout.Button("【wav】\nSoundsフォルダ内の\n全オーディオクリップに設定を適用する")) Execute(SoundFileType.WAV);
+        }
+
+        private static void Execute(SoundFileType type)
         {
-            ApplyOrThrow("MonoBGM", AudioClipLoadType.Streaming, AudioCompressionFormat.Vorbis, true);
-            ApplyOrThrow("MonoVoice", AudioClipLoadType.CompressedInMemory, AudioCompressionFormat.Vorbis, true);
-            ApplyOrThrow("MonoSE", AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.Vorbis, true);
-            ApplyOrThrow("MonoSERough", AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.ADPCM, true);
+            try
+            {
+                ApplyOrThrow("MonoBGM", type, AudioClipLoadType.Streaming, AudioCompressionFormat.Vorbis, true);
+                ApplyOrThrow("MonoVoice", type, AudioClipLoadType.CompressedInMemory, AudioCompressionFormat.Vorbis, true);
+                ApplyOrThrow("MonoSE", type, AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.Vorbis, true);
+                ApplyOrThrow("MonoSERough", type, AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.ADPCM, true);
 
-            ApplyOrThrow("StereoBGM", AudioClipLoadType.Streaming, AudioCompressionFormat.Vorbis, false);
-            ApplyOrThrow("StereoVoice", AudioClipLoadType.CompressedInMemory, AudioCompressionFormat.Vorbis, false);
-            ApplyOrThrow("StereoSE", AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.Vorbis, false);
-            ApplyOrThrow("StereoSERough", AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.ADPCM, false);
+                ApplyOrThrow("StereoBGM", type, AudioClipLoadType.Streaming, AudioCompressionFormat.Vorbis, false);
+                ApplyOrThrow("StereoVoice", type, AudioClipLoadType.CompressedInMemory, AudioCompressionFormat.Vorbis, false);
+                ApplyOrThrow("StereoSE", type, AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.Vorbis, false);
+                ApplyOrThrow("StereoSERough", type, AudioClipLoadType.DecompressOnLoad, AudioCompressionFormat.ADPCM, false);
 
-            "処理終了".Tell("00ffff");
+                "処理終了".Tell("00ffff");
+            }
+            catch (Exception e) { e.Error(); }
         }
-        catch (Exception e) { e.Error(); }
-    }
 
-    private static void ApplyOrThrow(string folderName, AudioClipLoadType loadType, AudioCompressionFormat compressionFormat, bool forceToMono)
-    {
-        string folderPath = Path.Combine(FOLDERS_ROOT_PATH, folderName);
-        if (Directory.Exists(folderPath) is false) throw new($"{folderPath}が存在しません");
-
-        string[] filePathes = Directory.GetFiles(folderPath, "*.mp3", SearchOption.AllDirectories);
-        foreach (string filePath in filePathes)
+        private static void ApplyOrThrow(string folderName, SoundFileType type, AudioClipLoadType loadType, AudioCompressionFormat compressionFormat, bool forceToMono)
         {
-            string assetPath = filePath.Replace(Application.dataPath, "").Replace('\\', '/');
-            AudioImporter importer = AssetImporter.GetAtPath(assetPath) as AudioImporter;
-            if (importer == null) continue;
+            string folderPath = Path.Combine(FOLDERS_ROOT_PATH, folderName);
+            if (Directory.Exists(folderPath) is false) throw new($"{folderPath}が存在しません");
 
-            AudioImporterSampleSettings settings = importer.defaultSampleSettings;
-            settings.loadType = loadType;
-            settings.compressionFormat = compressionFormat;
-            importer.defaultSampleSettings = settings;
-            importer.forceToMono = forceToMono;
+            string[] filePathes = Directory.GetFiles(folderPath, ToSearchPattern(type), SearchOption.AllDirectories);
+            foreach (string filePath in filePathes)
+            {
+                string assetPath = filePath.Replace(Application.dataPath, "").Replace('\\', '/');
+                AudioImporter importer = AssetImporter.GetAtPath(assetPath) as AudioImporter;
+                if (importer == null) continue;
 
-            importer.SaveAndReimport();
+                AudioImporterSampleSettings settings = importer.defaultSampleSettings;
+                settings.loadType = loadType;
+                settings.compressionFormat = compressionFormat;
+                importer.defaultSampleSettings = settings;
+                importer.forceToMono = forceToMono;
+
+                importer.SaveAndReimport();
+            }
         }
+
+        private enum SoundFileType
+        {
+            MP3,
+            WAV
+        }
+
+        private static string ToSearchPattern(SoundFileType type) => type switch
+        {
+            SoundFileType.MP3 => "*.mp3",
+            SoundFileType.WAV => "*.wav",
+            _ => string.Empty
+        };
     }
 }
 
