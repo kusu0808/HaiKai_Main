416799f299b38c93a91d2d7d8581678c43d39c91
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-11-17 21:32:55 +0900

    feature: カーソルホバー時にUIの色を変化させる処理

---
+++ Assets/_Projects/Scripts/SceneTitle/OnMousePointer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Title 
+{
+    public sealed class OnMousePointer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField, Header("カーソルホバー時に、影響を受けるUI")] private Image hoverImageUI;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (hoverImageUI != null)
+            {
+                hoverImageUI.color = Color.red;
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (hoverImageUI != null)
+            {
+                hoverImageUI.color = Color.white;
+            }
+        }
+
+    }
+}
+
+
+
