db6716446471a9123b4371ebede262d3b257cf30
haruki <tmhk1260@gmail.com>
2024-12-11 13:16:59 +0900

    refine: ヤツ娘声真似のフラグ修正

--- Assets/_Projects/Scripts/SceneMain/EventManager/ObserveBorderEntry/VillageFarWayYatsuDaughterVoice.cs
+++ Assets/_Projects/Scripts/SceneMain/EventManager/ObserveBorderEntry/VillageFarWayYatsuDaughterVoice.cs
@@ -8,7 +8,8 @@ namespace Main.EventManager
     {
         private async UniTaskVoid VillageFarWayYatsuDaughterVoice(CancellationToken ct)
         {
-            await UniTask.WaitUntil(() => _uiElements.KokeshiSecretKey.IsHolding() is true, cancellationToken: ct);
+            await UniTask.WaitUntil(() => _borders.IsFromUnderStageToShrineWayBorderEnabled is false,
+            cancellationToken: ct);
             await UniTask.WaitUntil(() => _borders.VillageFarWayYatsuDaughterVoice.IsIn(_player.Position) is true, cancellationToken: ct);
             _audioSources.GetNew().Raise(_audioClips.Voice.YaTsuImitateDaughterVoice, SoundType.Voice);
         }
