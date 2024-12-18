e460a7b1aa96096b890fd501fb5996655437f276
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-12-18 12:28:02 +0900

    feature: レティクルUI表示システム

---
+++ Assets/_Projects/Scripts/SceneMain/ReticleUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Main.Eventer.UIElements
+{
+    /// <summary>
+    /// レティクルUI　レイキャストが当たった際色を変更
+    /// </summary>
+    public sealed class ReticleUI : MonoBehaviour
+    {
+        [SerializeField] private Image _reticleImage;
+
+        public void ChangeColor(bool onRaycastHit)
+        {
+            if (_reticleImage == null) return;
+            _reticleImage.color = onRaycastHit ? Color.red : Color.white;
+        }
+    }
+}
