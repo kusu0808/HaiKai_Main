718489bfcb077eca673e996b9e58e312ff673b02
foriver4725 <hrt67@icloud.com>
2024-12-24 21:00:59 +0900

    refactor: 鎖をそれぞれ別個で管理するようにした

--- Assets/_Projects/Scripts/SceneMain/Eventer/Objects/DaughterChainClass.cs
+++ Assets/_Projects/Scripts/SceneMain/Eventer/Objects/DaughterChainClass.cs
@@ -8,19 +8,23 @@ namespace Main.Eventer.Objects
     public sealed class DaughterChainClass
     {
         [SerializeField, Required, SceneObjectsOnly]
-        private Collider[] _chains;
+        private PlacedItemClass _chain1;
+
+        [SerializeField, Required, SceneObjectsOnly]
+        private PlacedItemClass _chain2;
+
+        [SerializeField, Required, SceneObjectsOnly]
+        private PlacedItemClass _chain3;
+
+        public void Cut1() => _chain1.IsEnabled = false;
+        public void Cut2() => _chain2.IsEnabled = false;
+        public void Cut3() => _chain3.IsEnabled = false;
 
         public bool IsAllCut()
         {
-            if (_chains is null) return false;
-            if (_chains.Length <= 0) return false;
-
-            foreach (Collider chain in _chains)
-            {
-                if (chain == null) continue;
-                if (chain.gameObject.activeSelf) return false;
-            }
-
+            if (_chain1.IsEnabled) return false;
+            if (_chain2.IsEnabled) return false;
+            if (_chain3.IsEnabled) return false;
             return true;
         }
     }
