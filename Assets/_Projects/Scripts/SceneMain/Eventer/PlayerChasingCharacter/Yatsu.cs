cf428232ed31a2c783e8d47712225c992f3172c8
foriver4725 <hrt67@icloud.com>
2025-01-23 22:07:16 +0900

    bugfix: 2回目以降もチェイスBGMが鳴るように

--- Assets/_Projects/Scripts/SceneMain/Eventer/PlayerChasingCharacter/Yatsu.cs
+++ Assets/_Projects/Scripts/SceneMain/Eventer/PlayerChasingCharacter/Yatsu.cs
@@ -14,14 +14,13 @@ namespace Main.Eventer.PlayerChasingCharacter
         [SerializeField, Required, SceneObjectsOnly]
         private Animator _animator;
 
+        [SerializeField, Required, SceneObjectsOnly]
+        private AudioSource _audioSource;
+
         protected override float InitSpeed => 1.0f;
 
         public bool IsSteppingOnGlassShard { get; set; } = false;
         public AudioClip ChasedBGM { get; set; } = null;
-        private CancellationTokenSource _chasedBGMCts = null;
-
-        // 使い回すので、特別に生成
-        private AudioSource _audioSource = null;
 
         public Vector3 Position
         {
@@ -45,36 +44,17 @@ namespace Main.Eventer.PlayerChasingCharacter
 
         protected override void OnSpawn()
         {
-            base.OnSpawn();
             if (_animator != null) _animator.SetBool("IsMoving", true);
         }
 
         protected override void OnDespawn()
         {
-            base.OnDespawn();
             if (_animator != null) _animator.SetBool("IsMoving", false);
         }
 
         protected override void OnEnable()
         {
-            base.OnEnable();
-
-            if (_navMeshAgent != null) _audioSource = _navMeshAgent.gameObject.AddComponent<AudioSource>();
-
-            _chasedBGMCts = new CancellationTokenSource();
-            StartObservingChasedBGM(_chasedBGMCts.Token).Forget();
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-
-            _chasedBGMCts?.Cancel();
-            _chasedBGMCts?.Dispose();
-            _chasedBGMCts = null;
-
-            UnityEngine.Object.Destroy(_audioSource);
-            _audioSource = null;
+            StartObservingChasedBGM(_navMeshAgent.GetCancellationTokenOnDestroy()).Forget();
         }
 
         private async UniTaskVoid StartObservingChasedBGM(CancellationToken ct)
