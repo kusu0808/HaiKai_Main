bbf5ac61c9c1f51a694686ef001631a08c783d11
foriver4725 <hrt67@icloud.com>
2025-02-08 23:01:35 +0900

    feature: タイトルBGMがフェードインするように

--- Assets/_Projects/Scripts/SceneTitle/TitleBGM.cs
+++ Assets/_Projects/Scripts/SceneTitle/TitleBGM.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using General;
 using Sirenix.OdinInspector;
 using SO;
@@ -10,6 +13,14 @@ namespace Title
         [SerializeField, Required, SceneObjectsOnly]
         private AudioSource _audioSource;
 
-        private void Awake() => _audioSource.Raise(SAudioClips.Entity.BGM.Title, SoundType.BGM);
+        private void OnEnable() => Play(destroyCancellationToken).Forget();
+
+        private async UniTaskVoid Play(CancellationToken ct)
+        {
+            _audioSource.volume = 0;
+            _audioSource.Raise(SAudioClips.Entity.BGM.Title, SoundType.BGM);
+            await UniTask.WaitForSeconds(1, cancellationToken: ct);
+            await _audioSource.DOFade(1, 5).WithCancellation(ct);
+        }
     }
 }
